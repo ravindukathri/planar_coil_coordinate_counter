d240e389c379be62dcdd5fc406511dd288aec304
ravindukathri <ravindukathri@gmail.com>
2023-10-29 01:09:05 +0200

    updated

--- code.py
+++ code.py
@@ -25,8 +25,68 @@ try:
 except ValueError:
     print("Invalid input. Please enter a valid integer.")
 
+# first 5 numbers   
+list_x = [0,0,x,x,d+s]
+list_y = [0,y,y,0,0]
 
-calculate_coordinates(x, y, d, s, n)
+#Second Sets
+
+max = n
+i=1
+if max > 1:
+    while i<= max-1:
+        x1 = i*(d+s)
+        x2 = x-i*(d+s)
+        x3 = x-i*(d+s)
+        x4 = (i+1)*(d+s)
+        second_x = [x1,x2,x3,x4]
+        list_x = np.append(list_x,second_x)
+        
+        y1 = y-i*(d+s)
+        y2 = y-i*(d+s)
+        y3 = i*(d+s)
+        y4 = i*(s+d)
+        second_y = [y1,y2,y3,y4]
+        list_y = np.append(list_y,second_y)
+        
+        i+= 1
+    
+
+
+# Center Number
+center_x =  n*(d+s)
+center_y = (n-1)*s+n*d
+list_x = np.append(list_x,center_x)
+list_y = np.append(list_y, center_y)
+
+#Last Sets
+max = n
+i=1
+j=max-1
+if max > 1:
+    while j>= 1:
+        x5 = x-(j+1)*d-(j)*s
+        x6 = x-(j+1)*d-(j)*s
+        x7 = (j+1)*d+(j)*s
+        x8 = (j+1)*d+(j)*s
+        last_x = [x5,x6,x7,x8]
+        list_x = np.append(list_x,last_x)
+        
+        y5 = (j+1)*d+(j)*s
+        y6 = y-(j+1)*d-(j)*s
+        y7 = y-(j+1)*d-(j)*s
+        y8 = j*d+(j-1)*s
+        last_y = [y5,y6,y7,y8]
+        list_y = np.append(list_y,last_y)
+        
+        j -=1
+
+
+
+
+#last 5 numbers
+list_x = np.append(list_x, [x-d,x-d,d,d,0])
+list_y = np.append(list_y, [d,y-d,y-d,0,0])
 
 print(f"List X = {list_x}")
 print(f"List Y = {list_y}")
@@ -54,61 +114,3 @@ plt.grid()
 plt.show()
     
     
-def calculate_coordinates(x, y, d, s, n):
-    # First 5 numbers
-    list_x = [0, 0, x, x, x + d + s]
-    list_y = [0, y, y, 0, 0]
-
-    # Second Sets
-    max_val = n
-    i = 1
-    if max_val > 1:
-        while i <= max_val - 1:
-            x1 = i * (d + s)
-            x2 = x - i * (d + s)
-            x3 = x - i * (d + s)
-            x4 = (i + 1) * (d + s)
-            second_x = [x1, x2, x3, x4]
-            list_x.extend(second_x)
-
-            y1 = y - i * (d + s)
-            y2 = y - i * (d + s)
-            y3 = i * (d + s)
-            y4 = i * (s + d)
-            second_y = [y1, y2, y3, y4]
-            list_y.extend(second_y)
-
-            i += 1
-
-    # Center Number
-    center_x = n * (d + s)
-    center_y = (n - 1) * s + n * d
-    list_x.append(center_x)
-    list_y.append(center_y)
-
-    # Last Sets
-    max_val = n
-    j = max_val - 1
-    if max_val > 1:
-        while j >= 1:
-            x5 = x - (j + 1) * d - j * s
-            x6 = x - (j + 1) * d - j * s
-            x7 = (j + 1) * d + j * s
-            x8 = (j + 1) * d + j * s
-            last_x = [x5, x6, x7, x8]
-            list_x.extend(last_x)
-
-            y5 = (j + 1) * d + j * s
-            y6 = y - (j + 1) * d - j * s
-            y7 = y - (j + 1) * d - j * s
-            y8 = j * d + (j - 1) * s
-            last_y = [y5, y6, y7, y8]
-            list_y.extend(last_y)
-
-            j -= 1
-
-    # Last 5 numbers
-    list_x.extend([x - d, x - d, d, d, 0])
-    list_y.extend([d, y - d, y - d, 0, 0])
-
-    return list_x, list_y
